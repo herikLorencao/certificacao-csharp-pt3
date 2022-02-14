714542ca83c8540c518b1949d56baf20d8531c01
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-02-14 16:56:31 -0300

    adicionando tópico de classe base

---
+++ Topico1.Propriedades e Acessadores/Topico4.Classe Base/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Topico4.Classe_Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario(1500);
+            funcionario.CPF = "123.456.789-00";
+            funcionario.Nome = "josé da silva";
+            funcionario.DataNascimento = new DateTime(2000, 1, 1);
+
+            ((IFuncionario)funcionario).CargaHorariaMensal = 168;
+            ((IPlantonista)funcionario).CargaHorariaMensal = 32;
+            funcionario.EfeturarPagamento();
+            funcionario.CrachaGerado += (s, e) =>
+            {
+                Console.WriteLine("Crachá gerado");
+            };
+            ((IFuncionario)funcionario).GerarCracha();
+            ((IPlantonista)funcionario).GerarCracha();
+
+            Cliente cliente = new Cliente();
+            cliente.UltimaDataCompra = new DateTime(2020, 06, 04);
+            cliente.ValorUltimaCompra = new decimal(2000.50);
+        }
+    }
+    
+    interface IFuncionario
+    {
+        string CPF { get; set; }
+        string Nome { get; set; }
+        DateTime DataNascimento { get; set; }
+
+        event EventHandler CrachaGerado;
+
+        void GerarCracha();
+
+        decimal Salario { get; }
+        int CargaHorariaMensal { get; set; }
+
+        void EfeturarPagamento();
+    }
+
+    interface IPlantonista
+    {
+        int CargaHorariaMensal { get; set; }
+        void GerarCracha();
+    }
+
+    class Funcionario : Pessoa, IFuncionario, IPlantonista
+    {
+        public event EventHandler CrachaGerado;
+
+        void IFuncionario.GerarCracha()
+        {
+            if (CrachaGerado != null)
+            {
+                CrachaGerado(this, new EventArgs());
+            }
+        }
+
+        void IPlantonista.GerarCracha()
+        {
+            if (CrachaGerado != null)
+            {
+                CrachaGerado(this, new EventArgs());
+            }
+        }
+
+        public decimal Salario { get; }
+
+        int IFuncionario.CargaHorariaMensal { get; set; }
+
+        int IPlantonista.CargaHorariaMensal { get; set; }
+
+        public Funcionario(decimal salario)
+        {
+            Salario = salario;
+        }
+
+        public void EfeturarPagamento()
+        {
+            Console.WriteLine("Pagamento Efetuado");
+        }
+    }
+
+    sealed class Cliente : Pessoa
+    {
+        public DateTime UltimaDataCompra { get; set; }
+        public decimal ValorUltimaCompra { get; set; }
+    }
+
+    abstract class Pessoa
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
